c6cdd956dd4f4bcb147319737d69dedaf9ed2e2f
Karthik Shanmugam <karthik.shambu@gmail.com>
2015-04-09 16:28:04 +0530

    Flash messages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,31 @@
 class HomeController < ApplicationController
 
   def index
+    flash[:notice] = ""
+    flash[:error] = ""
     render :layout => 'application'
   end
 
   def send_sms
-    SummerPhoneNumber.create(:phone_number => params[:phone_no])
-    phone_number = "91#{params[:phone_no]}"
-    content = SmsContent.first.content
-    Exotel::Sms.send(:from => Settings.exotel_number, :to => phone_number, :body => content)
-    flash[:notice] = "You'll receive the Link as an SMS."
-    render :action => "index"
+    summer_phone_number = SummerPhoneNumber.new(:phone_number => params[:phone_no])
+    if summer_phone_number.save
+      phone_number = "91#{params[:phone_no]}"
+      content = SmsContent.first.content
+      sms_sent_count = SummerPhoneNumber.where('phone_number = ? and trunc(created_at) = ?', params[:phone_no], Time.zone.today).count
+      notice = ""
+      if sms_sent_count < 3
+        Exotel::Sms.send(:from => Settings.exotel_number, :to => phone_number, :body => content)
+        flash[:notice] = "You'll receive a SMS"
+      else
+        flash[:notice] = "We already sent you an SMS. Try Tomorrow!"
+      end
+      flash[:error] = ""
+      render :action => "index", :notice=> flash[:notice]
+    else
+      flash[:notice] = ""
+      flash[:error] = summer_phone_number.errors.messages[:phone_number]
+      render :action => "index", :error => flash[:error]
+    end
   end
 
 end
--- app/models/summer_phone_number.rb
+++ app/models/summer_phone_number.rb
@@ -1,3 +1,6 @@
 class SummerPhoneNumber < ActiveRecord::Base
   attr_accessible :phone_number
+  validates :phone_number, format: { with: /[0-9]{10}/,
+    message: "Phone Number should be valid." }
+  # validates :phone_number, length: { is: 10 }
 end
